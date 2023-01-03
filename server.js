81ca56aaa0a9185d06eb260bfeaf27cf509a5dfc
MR-Khaleghi <rasool_khe@yahoo.com>
2023-01-03 18:24:38 +0330

    amended package.json

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+// import data from './data.js';
+import mongoose from 'mongoose';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
+import orderRouter from './routes/orderRoutes.js';
+import uploadRouter from './routes/uploadRoute.js';
+import path from 'path';
+
+mongoose
+  .connect('mongodb://localhost/ecommerce')
+  .then(() => console.log('connected mongo'))
+  .catch((err) => console.log("coudn't connect to mongo"));
+
+const app = express();
+// these 2 are used by post
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/uploads', uploadRouter);
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+// console.log(path.join(__dirname, '/uploads'));
+// async-handler
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
